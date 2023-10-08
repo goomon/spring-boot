9d02400e4075d7b4dd2755688ace4d2cf758f66e
goomon <jaegoo.jho@gmail.com>
2023-10-08 20:11:08 +0900

    컴포넌트 스캔 적용

--- src/main/java/com/github/goomon/boot/service/SimpleHelloService.java
+++ src/main/java/com/github/goomon/boot/service/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package com.github.goomon.boot.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name) {
