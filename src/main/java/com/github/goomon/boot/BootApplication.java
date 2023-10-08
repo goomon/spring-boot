e2a00562b3867869ea276b08e2d51d7b8d44716e
goomon <jaegoo.jho@gmail.com>
2023-10-08 13:47:15 +0900

    수동으로 톰캣 서버 실행하기

--- src/main/java/com/github/goomon/boot/BootApplication.java
+++ src/main/java/com/github/goomon/boot/BootApplication.java
@@ -1,8 +1,16 @@
 package com.github.goomon.boot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+
 public class BootApplication {
 
 	public static void main(String[] args) {
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+
+		// Start tomcat servlet container.
+		webServer.start();
 	}
 
 }
