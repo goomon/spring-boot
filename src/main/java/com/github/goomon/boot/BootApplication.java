1c34233386d4999ecedabc4cb845aadca4eb8e29
goomon <jaegoo.jho@gmail.com>
2023-10-08 14:19:53 +0900

    프론트 컨트롤러로 전환

--- src/main/java/com/github/goomon/boot/BootApplication.java
+++ src/main/java/com/github/goomon/boot/BootApplication.java
@@ -3,6 +3,7 @@ package com.github.goomon.boot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -17,16 +18,23 @@ public class BootApplication {
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("front-controller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					// authentication, security, multi-lang, commons, ...
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello " + name);
+					} else if (req.getRequestURI().equals("/users") && req.getMethod().equals(HttpMethod.GET.name())) {
+						//
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
         });
 
 		// Start tomcat servlet container.
