0ce24cc7fc08a13c7a18fa6156c48aede0f1ed8a
goomon <jaegoo.jho@gmail.com>
2023-10-08 20:28:54 +0900

    `MySpringApplication` 생성

--- src/main/java/com/github/goomon/boot/BootApplication.java
+++ src/main/java/com/github/goomon/boot/BootApplication.java
@@ -1,12 +1,10 @@
 package com.github.goomon.boot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -23,24 +21,7 @@ public class BootApplication {
 	}
 
 	public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-				WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("dispatcherServlet",
-								new DispatcherServlet(this)
-						).addMapping("/*"));
-
-				// Start tomcat servlet container.
-				webServer.start();
-			}
-		};
-		applicationContext.register(BootApplication.class);
-		applicationContext.refresh();
+		MySpringApplication.run(BootApplication.class, args);
 	}
 
 }
