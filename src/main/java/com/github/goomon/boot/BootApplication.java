27f06e5f098baf9cd630353ec4ed51bd001ffe0a
goomon <jaegoo.jho@gmail.com>
2023-10-08 18:24:30 +0900

    서블릿 컨테이너 생성을 스프링 초기화 과정에서 실행하도록 변경

--- src/main/java/com/github/goomon/boot/BootApplication.java
+++ src/main/java/com/github/goomon/boot/BootApplication.java
@@ -11,18 +11,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class BootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("dispatcherServlet",
+								new DispatcherServlet(this)
+						).addMapping("/*"));
+
+				// Start tomcat servlet container.
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("dispatcherServlet",
-						new DispatcherServlet(applicationContext)
-				).addMapping("/*"));
-
-		// Start tomcat servlet container.
-		webServer.start();
 	}
 
 }
