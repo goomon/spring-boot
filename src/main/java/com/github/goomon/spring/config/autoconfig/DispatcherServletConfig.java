ea36c53e0925d16bdbe8d11eadb50dd8e4abc83c
goomon <jaegoo.jho@gmail.com>
2023-10-09 17:59:40 +0900

    `proxyBeanMethods = false` 수정

--- src/main/java/com/github/goomon/spring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/com/github/goomon/spring/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package com.github.goomon.spring.config.autoconfig;
 
+import com.github.goomon.spring.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
