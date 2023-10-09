423e9acfd5c290a317f69a4dd767c206c4250c65
goomon <jaegoo.jho@gmail.com>
2023-10-09 16:18:01 +0900

    합성 애노테이션 적용(`@MySpringBootAnnotation`)

--- src/main/java/com/github/goomon/boot/BootApplication.java
+++ src/main/java/com/github/goomon/boot/BootApplication.java
@@ -1,25 +1,10 @@
 package com.github.goomon.boot;
 
+import com.github.goomon.boot.annotation.MySpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootApplication
 public class BootApplication {
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
-	}
-
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
