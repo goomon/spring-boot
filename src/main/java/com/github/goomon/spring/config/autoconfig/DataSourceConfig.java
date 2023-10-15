be8884b8bf727dc058195bdc9421698482c8b8a4
goomon <jaegoo.jho@gmail.com>
2023-10-15 21:42:42 +0900

    SimpleDriverDataSource 자동구성 정의하기

---
+++ src/main/java/com/github/goomon/spring/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.github.goomon.spring.config.autoconfig;
+
+import com.github.goomon.spring.config.ConditionalMyOnClass;
+import com.github.goomon.spring.config.EnableMyConfigurationProperties;
+import com.github.goomon.spring.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+}
